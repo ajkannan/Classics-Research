fbe2c7f5489972d0dabb7205949da74fc5199a47
JamesBrofos <RedArk@redarks-mbp.wireless.dartmouth.edu>
2014-03-11 12:21:12 -0400

    classics initial

---
+++ Utilities/FunctionalNGram.py
@@ -0,0 +1,77 @@
+from Text import Text
+
+from pprint import pprint
+import numpy as np
+import string
+import itertools
+
+class FunctionalNGram(object):
+	"""docstring for FunctionalNGram"""
+	def __init__(self, text):
+		super(FunctionalNGram, self).__init__()
+		
+		if type(text) is not Text:
+			raise Exception("Invalid input to FunctionalNGram class. Only provide inputs of type Text.")
+
+		self.text = text
+		self.find_n_grams(2)
+
+		self.compute_probability_features()
+
+	def find_n_grams(self, n):
+		n_grams = self.initialize_n_grams()
+		text = self.text.processed_text
+
+		for line in text:
+			joined_line = " ".join(line)
+
+			for i in xrange(len(joined_line) - 1):
+				n_gram = tuple([joined_line[i + j] for j in xrange(n)])
+				
+				if n_gram in n_grams.keys():
+
+					n_grams[n_gram] += 1.0
+
+		self.n_grams = n_grams
+
+
+	def initialize_n_grams(self):
+		n_grams = {}
+		letters = ' ' + string.ascii_lowercase
+
+		for letter_1 in letters:
+			for letter_2 in letters:
+				n_grams[(letter_1, letter_2)] = 0.0
+
+		return n_grams
+
+	def compute_probability_features(self):
+		n_gram_tuples = self.n_grams.keys()
+
+		n_gram_probabilities = {}
+		alphabet = {}
+	
+		for n_gram in n_gram_tuples:
+			
+			if n_gram[0] not in alphabet:
+				frequency_count = 0.0
+
+				for letter_gram in n_gram_tuples:
+					if n_gram[0] == letter_gram[0]:
+						frequency_count += self.n_grams[letter_gram]
+
+				alphabet[n_gram[0]] = frequency_count
+
+			if alphabet[n_gram[0]] > 0:
+				n_gram_probabilities[n_gram] = self.n_grams[n_gram] / alphabet[n_gram[0]]
+			else:
+				n_gram_probabilities[n_gram] = 0.0
+
+			
+		pprint(n_gram_probabilities)
+
+
+
+
+text = Text("../Texts/agamemnon.txt")
+fng = FunctionalNGram(text)
---
+++ Utilities/Text.py
@@ -0,0 +1,84 @@
+
+from pprint import pprint
+import re
+
+class Text(object):
+	"""docstring for Text"""
+	def __init__(self, file_path):
+		super(Text, self).__init__()
+
+		self.open_file_path(file_path)
+		self.preprocess_raw_text()
+		self.concatenate_processed_text()
+
+	def open_file_path(self, file_path):
+		text = []
+		with open(file_path, "r") as f:
+			for line in f:
+				text.append(line)
+
+		self.raw = text
+
+	def preprocess_raw_text(self, lower_case = True, punctuation = "delete"):
+		processed_text = []
+		for line in self.raw:
+			processed_line = self.process_line(line, lower_case, punctuation)
+			if len(processed_line):
+				processed_text.append(processed_line)
+
+		self.processed_text = processed_text
+
+	def process_line(self, line, lower_case, punctuation):
+		# Removes line numbers and other numeric strings from this line of
+		# the play.
+		processed_line = [word for word in line.split() if not word.isdigit()]
+
+
+		# Removes the speaker indicator from this particular line. Speakers are
+		# indicated by the convention "[SPEAKER]". If the option is specified as
+		# True, then these remaining words are also converted to their lower 
+		# case form in this step.
+		processed_line = [word.lower() if lower_case else word for word in processed_line if word[0] != "[" and word[-1] != "]"]
+
+	
+		if punctuation == "separate":
+			# In the event that we wish to separate punctuation from neighboring 
+			# words, then we employ the regular expression tool to create the 
+			# separated list.
+			processed_line = re.findall(u"[\w']+|[.,!?;]", " ".join(processed_line))
+
+		elif punctuation == "delete":
+			# In the event that we wish to delete punctation from the text, 
+			# we employ the regular expressions tool to separate punctuation and 
+			# subsequently remove punctuation from the resulting list.
+			punctuation_set = set([".", ",", "!", "?", ";"])
+
+			separated_punctuation = re.findall(u"[\w']+|[.,!?;]", " ".join(processed_line))
+			punctuation_removed = ["".join(c for c in s if c not in punctuation_set) for s in separated_punctuation]
+			processed_line = [word for word in punctuation_removed if word != ""]
+
+		return processed_line
+
+	def concatenate_processed_text(self):
+		concatenated_text = ""
+		for line in self.processed_text:
+			concatenated_text += " ".join(line) + " "
+
+
+		# Remove the trailing space character from the concatenated string
+		# of words.
+		concatenated_text = concatenated_text[:-1]
+
+		self.concatenated_text = concatenated_text
+
+
+# text = Text("../Texts/agamemnon.txt")
+
+# text.preprocess_raw_text()
+# text.concatenate_processed_text()
+
+# pprint(text.processed_text)
+# pprint(text.concatenated_text)
+
+
+
