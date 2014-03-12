6826e253ac637da2b6229914bc84d88a4ffa00d3
Ajay Kannan <ajay.kannan93@gmail.com>
2014-03-12 00:18:38 -0400

    generalized FunctionalNGram to handle more than bigrams

--- Utilities/FunctionalNGram.py
+++ Utilities/FunctionalNGram.py
@@ -4,8 +4,19 @@ from pprint import pprint
 import numpy as np
 import string
 import itertools
+import copy
 
 def combine_n_gram_features(n_gram_probabilities):
+	""" Compiles n-gram probability features into numpy array
+
+	Parameters
+	----------
+	n_gram_probabilities:
+
+	Returns
+	-------
+	features and respective n-grams
+	"""
 	if type(n_gram_probabilities) is not list:
 		raise Exception("Invalid input to method. Only provide a list object.")
 
@@ -28,57 +39,116 @@ def combine_n_gram_features(n_gram_probabilities):
 
 
 class FunctionalNGram(object):
-	"""docstring for FunctionalNGram"""
-	def __init__(self, text):
+	""" Represents the functional n-gram feature representation of the text"""
+	def __init__(self, text, n = 2):
+		""" Calls functions to calculate probabilities of n-grams
+
+		Parameters
+		----------
+		n: size of lettergram
+
+		Returns
+		-------
+		None
+		"""
+
 		super(FunctionalNGram, self).__init__()
-		
+
 		if type(text) is not Text:
 			raise Exception("Invalid input to FunctionalNGram class. Only provide inputs of type Text.")
 
 		self.text = text
-		self.find_n_grams()
-
+		self.find_n_grams(n)
 		self.compute_probability_features()
 
-	def find_n_grams(self):
-		n_grams = self.initialize_n_grams()
+
+	def find_n_grams(self, n):
+		""" Iterates through text finding frequencies of n-grams
+
+		Parameters
+		----------
+		n: size of lettergram
+
+		Returns
+		-------
+		None
+		"""
+
+		n_grams = self.initialize_n_grams(n)
 		text = self.text.processed_text
-		n = 2
 
 		for line in text:
 			joined_line = " ".join(line)
-
-			for i in xrange(len(joined_line) - 1):
+			for i in xrange(len(joined_line) - n + 1):
 				n_gram = tuple([joined_line[i + j] for j in xrange(n)])
-				
 				if n_gram in n_grams.keys():
-
 					n_grams[n_gram] += 1.0
 
 		self.n_grams = n_grams
 
 
-	def initialize_n_grams(self):
-		n_grams = {}
-		letters = ' ' + string.ascii_lowercase
+	def initialize_n_grams(self, n):
+		""" Initializes n-gram dictionary.
 
+		Keys of the dictionary are all possible n-grams of alphabetical letters
+		and spaces (punctuation not included). Values in the dictionary are the
+		respective frequencies.
+
+		Parameters
+		----------
+		n: size of lettergram
+
+		Returns
+		-------
+		dictionary mentioned above
+		"""
+
+		n_grams = {}
+		keys = []
+		letters = ' ' + string.ascii_lowercase
 		for letter_1 in letters:
-			for letter_2 in letters:
-				n_grams[(letter_1, letter_2)] = 0.0
+			keys.append([letter_1])
+
+		for i in xrange(1, n):
+			# Add next character to each key
+			keys_old_copy = copy.copy(keys)
+			for key in keys_old_copy:
+				for letter in letters:
+					new_key = copy.copy(key)
+					new_key.append(letter)
+					keys.append(new_key)
+
+			# Remove shorter keys
+			keys_old_copy = copy.copy(keys)
+			for key in keys_old_copy:
+				if len(key) < i + 1:
+					keys.remove(key)
+
+		for key in keys:
+			n_grams[tuple(key)] = 0.0
 
 		return n_grams
 
-	def compute_probability_features(self):
-		n_gram_tuples = self.n_grams.keys()
 
+	def compute_probability_features(self):
+		""" Computes probability features
+
+		Parameters
+		----------
+		None
+
+		Returns
+		-------
+		None
+		"""
+
+		n_gram_tuples = self.n_grams.keys()
 		n_gram_probabilities = {}
 		alphabet = {}
-	
+
 		for n_gram in n_gram_tuples:
-			
 			if n_gram[0] not in alphabet:
 				frequency_count = 0.0
-
 				for letter_gram in n_gram_tuples:
 					if n_gram[0] == letter_gram[0]:
 						frequency_count += self.n_grams[letter_gram]
@@ -93,4 +163,6 @@ class FunctionalNGram(object):
 
 		self.probability_features = n_gram_probabilities
 
-		
+
+if __name__ == "__main__":
+	n_gram = FunctionalNGram()
--- functional_n_gram_one_class_svm.py
+++ functional_n_gram_one_class_svm.py
@@ -16,11 +16,11 @@ def main():
 	files = [f for f in listdir(path) if isfile(join(path, f))]
 	pprint(files)
 
-	# Extract n gram probability features from the texts and 
-	# construct them as a numpy 2D array. We also returns the 
-	# n grams under consideration in the experiments here, 
+	# Extract n gram probability features from the texts and
+	# construct them as a numpy 2D array. We also returns the
+	# n grams under consideration in the experiments here,
 	# although we have no particular use for them.
-	features, n_grams = combine([FNG(Text(path + f)) for f in files])
+	features, n_grams = combine([FNG(Text(path + f), 3) for f in files])
 
 	apply_pca = False
 
@@ -36,7 +36,7 @@ def main():
 			"test" : features[[1, 3], :]
 			}
 
-	
+
 	# Unfortunately, it does not appear to be possible to derive a perfect
 	# accuracy solution in the grid search specified below. However, it is
 	# provided here anyway for educational purposes.
@@ -54,7 +54,7 @@ def main():
 						"train" : clf.predict(x["train"]),
 						"test" : clf.predict(x["test"])
 						}
-				
+
 
 					if all(y["train"] == 1.0) and all(y["test"] == -1.0):
 						pprint({"nu" : nu, "gamma" : gamma, "y" : y, "kernel" : kernel})
